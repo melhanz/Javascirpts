78c0088962d680c4a3ac761769a96513310778a7
1768375 <melryan.rosagaran@gmail.com>
2020-05-21 20:48:03 +0800

    Javascriptis functioning

---
+++ calculatorjs/app.js
@@ -0,0 +1,8 @@
+const calc = document.querySelector(".calc-form");
+// const keys = document.getElementsByClassName(".keys");
+
+function myClick(){
+    var key = document.getElementById("btn").textContent;
+    console.log(key);
+    
+}
